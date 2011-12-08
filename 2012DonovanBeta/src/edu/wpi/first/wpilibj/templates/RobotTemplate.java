39965295bfaa02b6f12c3e8c46c663db0e1fc913
unknown <admin@LLR4MH17.(none)>
2011-12-08 17:12:09 -0500

    Did thie things

--- 2012DonovanBeta/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ 2012DonovanBeta/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -9,6 +9,7 @@ package edu.wpi.first.wpilibj.templates;
 
 
 import edu.wpi.first.wpilibj.SimpleRobot;
+import edu.wpi.first.wpilibj.*;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -18,17 +19,50 @@ import edu.wpi.first.wpilibj.SimpleRobot;
  * directory.
  */
 public class RobotTemplate extends SimpleRobot {
+      Joystick lstick;
+    Joystick rstick;
+    Joystick shootStick;
+    Joystick gamepad;
+    Gyro george;
+
+
+    Victor acquirer;
+   
+    public RobotTemplate() {
+        lstick = new Joystick(1); //usb port
+        rstick = new Joystick(2); //usb port
+        shootStick = new Joystick(3); //usb portRIVE_3_CHANNEL, DRIVE_4_CHANNEL); //digital channelss
+        george = new Gyro(1,1);
+        gamepad = new Joystick(4);
+        acquirer = new Victor(2,9);
+        System.out.println("contsructed Donovan");
+    }
+    public void operatorControl() {
+        getWatchdog().setEnabled(false);
+        while (isOperatorControl() && isEnabled()) {
+            Timer.delay(.01);
+            
+            System.out.println("hello?  In operator control.");
+         
+        }
+    }
+
     /**
      * This function is called once each time the robot enters autonomous mode.
      */
     public void autonomous() {
         
+        acquirer.set(.2);
+        Timer red = new Timer();
+        
+        
+        while(isAutonomous()){
+            System.out.println(george.getAngle());
+        }
     }
 
-    /**
-     * This function is called once each time the robot enters operator control.
-     */
-    public void operatorControl() {
-
+    public void doNoHarm() {
     }
-}
+
+    public void obeyOrders() {
+    }}
